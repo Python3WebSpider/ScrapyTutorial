c6888be37634085a50cf29e67aea71a0e30e2e99
Germey <cqc@cuiqingcai.com>
2020-08-29 20:13:15 +0800

    update v2

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ScrapyTutorial
 Scrapy Tutorial
+
+《Python3网络爬虫开发实战（第一版）》见 v1 分支。
+《Python3网络爬虫开发实战（第二版）》见 master 分支。
--- tutorial/pipelines.py
+++ tutorial/pipelines.py
@@ -5,7 +5,7 @@ from scrapy.exceptions import DropItem
 class TextPipeline(object):
     def __init__(self):
         self.limit = 50
-
+    
     def process_item(self, item, spider):
         if item['text']:
             if len(item['text']) > self.limit:
@@ -15,26 +15,26 @@ class TextPipeline(object):
             return DropItem('Missing Text')
 
 
-class MongoPipeline(object):
-    def __init__(self, mongo_uri, mongo_db):
-        self.mongo_uri = mongo_uri
-        self.mongo_db = mongo_db
-
+class MongoDBPipeline(object):
+    def __init__(self, connection_string, database):
+        self.connection_string = connection_string
+        self.database = database
+    
     @classmethod
     def from_crawler(cls, crawler):
         return cls(
-            mongo_uri=crawler.settings.get('MONGO_URI'),
-            mongo_db=crawler.settings.get('MONGO_DB')
+            connection_string=crawler.settings.get('MONGODB_CONNECTION_STRING'),
+            database=crawler.settings.get('MONGODB_DATABASE')
         )
-
+    
     def open_spider(self, spider):
-        self.client = pymongo.MongoClient(self.mongo_uri)
-        self.db = self.client[self.mongo_db]
-
+        self.client = pymongo.MongoClient(self.connection_string)
+        self.db = self.client[self.database]
+    
     def process_item(self, item, spider):
         name = item.__class__.__name__
         self.db[name].insert(dict(item))
         return item
-
+    
     def close_spider(self, spider):
         self.client.close()
